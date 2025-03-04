499735f08106efdb0eadb712d8a09d927ec7eb53
ERDASSS <com123465@mail.ru>
2025-03-04 15:59:55 +0500

    first task

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {readLine} = require('./console');
 const files = getFiles();
 
 console.log('Please, write your command!');
-readLine(processCommand);
+processCommand('show')
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
@@ -16,10 +16,32 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            const ans = getTODOComments(getFiles());
+            console.log(ans)
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
+
+function getTODOComments(files) {
+    let arrStr = [];
+    for (const file of files) {
+        const code = file.split('\n');
+        for (const str of code) {
+            const split = str.split('//');
+
+            if ((split.length > 1 || str.slice(0, 2) === '//') && split[1].slice(1, 5) == 'TODO') {
+                arrStr.push('//' + split[1]);
+            }
+        }
+    }
+
+    return arrStr;
+}
+
+
 
 // TODO you can do it!
